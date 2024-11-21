760cc01630f2af1cdce92c13253a3124a94c7d0c
liukatkat <katrinaliutinnam@gmail.com>
2024-11-21 16:10:15 -0500

    move type to new file

---
+++ src/TSType.hs
@@ -0,0 +1,30 @@
+module TSType where
+
+import Data.Map (Map)
+import Data.Map qualified as Map
+
+type TSGlobalEnv = Map String TSType
+type TSLocalEnv = Map String TSType
+type TSObjectEnv = Map String TSType
+
+data TSTypeEnv = TSTypeEnv {
+  globalEnv :: TSGlobalEnv,
+  localEnv :: TSLocalEnv,
+  objectEnv :: TSObjectEnv
+}
+
+data TSType
+  = TBoolean -- boolean
+  | TNumber -- number
+  | TString -- string
+  | TArray TSType -- Array<T>
+  | TTuple TSType TSType -- [T, U]
+  | TEnum [(String, TSType)]
+  | TUnknown
+  | TAny
+  | TVoid
+  | TNull
+  | TUndefined
+  | TNever
+  | TObject (Map String TSType)
+  | TFunction [TSType] TSType
--- src/TSTypeChecker.hs
+++ src/TSTypeChecker.hs
@@ -1,35 +1,9 @@
 module TSTypeChecker where
 
 import TSError (Error)
+import TSType
 import TSSyntax
-import Data.Map (Map)
-import Data.Map qualified as Map
 
-type TSGlobalEnv = Map String TSType
-type TSLocalEnv = Map String TSType
-type TSObjectEnv = Map String TSType
-
-data TSTypeEnv = TSTypeEnv {
-  globalEnv :: TSGlobalEnv,
-  localEnv :: TSLocalEnv,
-  objectEnv :: TSObjectEnv
-}
-
-data TSType
-  = TBoolean -- boolean
-  | TNumber -- number
-  | TString -- string
-  | TArray TSType -- Array<T>
-  | TTuple TSType TSType -- [T, U]
-  | TEnum [(String, TSType)]
-  | TUnknown
-  | TAny
-  | TVoid
-  | TNull
-  | TUndefined
-  | TNever
-  | TObject (Map String TSType)
-  | TFunction [TSType] TSType
 
 typeCheckExpr :: Expr -> State TSTypeEnv (Either Error TSType)
 typeCheckExpr = undefined
@@ -37,5 +11,5 @@ typeCheckExpr = undefined
 typeCheckStmt :: Stmt -> State TSTypeEnv (Either Error ())
 typeCheckStmt = undefined
 
-typeCheckProgram :: Block -> Either Error (Map String TSType)
+typeCheckProgram :: Block -> Either Error TSGlobalEnv
 typeCheckProgram = undefined
