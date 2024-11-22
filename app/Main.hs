eff3cf482d2fa5fdf75d5537faec9e29a4fae9af
Eunsoo Shin <me@esinx.net>
2024-11-22 10:03:02 -0500

    main

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,24 @@
 module Main where
 
 import FromTS (fromTS)
+import System.Environment qualified as Env
+import System.IO qualified as IO
+import System.IO.Error qualified as IO
+import TSError (TSError, fromTSError)
 
 main :: IO ()
-main = 
+main = do
+  args <- Env.getArgs
+  case args of
+    [filename] -> do
+      result <- fromTSFile filename
+      case result of
+        Right js -> putStrLn js
+        Left err -> putStrLn $ fromTSError err
+    _ -> putStrLn "Usage: fromTS <filename>"
+
+fromTSFile :: String -> IO (Either TSError String)
+fromTSFile filename = do
+  handle <- IO.openFile filename IO.ReadMode
+  str <- IO.hGetContents handle
+  pure $ fromTS str
