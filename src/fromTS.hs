1b8307f3be217d3d5a9d52bd4bed217a50d4ed4c
Eunsoo Shin <me@esinx.net>
2024-12-12 19:40:04 -0500

    feat(fromTS): main

--- src/fromTS.hs
+++ src/fromTS.hs
@@ -1,7 +1,27 @@
 module FromTS where
 
-import TSError (Error)
+import Data.Map
+import TSError (Error (SyntaxError, TypeError))
+import TSParser
+import TSSyntax
+import TSTranspiler
+import TSType
+import TSTypeChecker (typeCheckProgram)
+import Prelude
 
 -- | Transpile a TypeScript source into JavaScript
-fromTS :: String -> Either Error String
-fromTS = undefined
+fromTS :: String -> IO (Either Error String)
+fromTS fileName = do
+  result <- introspectTS fileName
+  case result of
+    Left err -> return $ Left err
+    Right (ts, _) -> return $ Right (transpileTS ts)
+
+introspectTS :: String -> IO (Either Error (Block, Map String TSType))
+introspectTS fileName = do
+  parsed <- parseTSFile fileName
+  case parsed of
+    Left err -> return $ Left $ SyntaxError (show err)
+    Right ts -> case typeCheckProgram ts of
+      Left err -> return $ Left $ TypeError (show err)
+      Right typeMap -> return $ Right (ts, typeMap)
